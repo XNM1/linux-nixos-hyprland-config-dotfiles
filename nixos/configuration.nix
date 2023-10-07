69a857ef515aa5701edf7e079ebde7cfb5327677
XNM <artem.shv@proton.me>
2023-10-07 21:59:02 +0300

    changed theme names for compatibility with the latest system update

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -88,8 +88,8 @@
   # };
 
   # Enable Theme
-  environment.variables.GTK_THEME = "Catppuccin-Macchiato-Standard-Teal-dark";
-  environment.variables.XCURSOR_THEME = "Catppuccin-Macchiato-Teal-Cursors";
+  environment.variables.GTK_THEME = "Catppuccin-Macchiato-Standard-Teal-Dark";
+  environment.variables.XCURSOR_THEME = "Catppuccin-Macchiato-Teal";
   environment.variables.XCURSOR_SIZE = "24";
   console = {
     earlySetup = true;
