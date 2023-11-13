e6e9aca4990bf8a0f6edc4d6983c16aa9b7c301b
XNM <artem.shv@proton.me>
2023-11-13 20:40:01 +0200

    added 'cl' alias to 'clear' command

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -2,6 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+alias cl="clear"
 alias conf="z ~/.config"
 alias nixos="z /etc/nixos"
 alias store="z /nix/store"
