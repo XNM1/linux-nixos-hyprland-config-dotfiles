e94f33f4b0c8d14e06a9eb7dc4cb20eb1058ed93
xnm <artem.shv@proton.me>
2024-04-06 01:26:01 +0300

    airplane-mode bugfix

--- home/.config/fish/functions/bluetooth_toggle.fish
+++ home/.config/fish/functions/bluetooth_toggle.fish
@@ -1,9 +1,13 @@
 function bluetooth_toggle
     set bluetooth_status (rfkill list bluetooth | grep -i -o "Soft blocked: yes")
+    set backup_file ~/.cache/airplane_backup
 
     if [ -z "$bluetooth_status" ]
         rfkill block bluetooth
     else
         rfkill unblock bluetooth
+        if test -e $backup_file
+            rm $backup_file
+        end
     end
 end
--- home/.config/fish/functions/wifi_toggle.fish
+++ home/.config/fish/functions/wifi_toggle.fish
@@ -1,9 +1,13 @@
 function wifi_toggle
     set wifi_status (nmcli radio wifi)
+    set backup_file ~/.cache/airplane_backup
 
-    if [ "$wifi_status" = "enabled" ]
+    if [ "$wifi_status" = enabled ]
         nmcli radio wifi off
     else
         nmcli radio wifi on
+        if test -e $backup_file
+            rm $backup_file
+        end
     end
 end
