4f283db288c73a127bb78d588efd736418c77a3e
xnm <artem.shv@proton.me>
2025-02-03 21:08:01 +0200

    update: Refactor ollama, Add searxng & open-webui services

--- nixos/llm.nix
+++ nixos/llm.nix
@@ -1,20 +1,39 @@
 { pkgs, ... }:
 
 {
-  services.ollama.enable = true;
-  services.ollama.loadModels = [ "llama3.2:3b" "llama3.2-vision:11b" "phi4:14b" "qwq:32b" "dolphin3:8b" "smallthinker:3b" "nomic-embed-text" ];
-  services.ollama.acceleration = "cuda";
+
+  services.ollama = {
+    enable = true;
+    loadModels = [ "llama3.2:3b" "llama3.2-vision:11b" "phi4:14b" "deepseek-r1:7b" "dolphin3:8b" "smallthinker:3b" "nomic-embed-text" ];
+    acceleration = "cuda";
+  };
+
+  services.searx = {
+    enable = true;
+    settings = {
+      server = {
+        port = 7777;
+        bind_address = "127.0.0.1";
+        secret_key = "your_key_here"; # FIXME: generate and paste your custom key here
+      };
+    };
+  };
+
+  services.open-webui = {
+    enable = true;
+    port = 8888;
+    host = "127.0.0.1";
+  };
   
   environment.systemPackages = with pkgs; [
     oterm
-    open-webui
     alpaca
-    nextjs-ollama-llm-ui
-
     aichat
     aider-chat
 
-    tgpt
-    smartcat
+    # tgpt
+    # smartcat
+    # nextjs-ollama-llm-ui
+    # open-webui
   ];
 }
