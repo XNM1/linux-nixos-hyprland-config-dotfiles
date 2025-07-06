4b2884451c40b1703a6c6f01966a2335f58d7c8f
xnm <artem.shv@proton.me>
2025-07-06 16:45:56 +0300

    feat(ollama): 🚀 Update Gemma3 model variant in loadModels
    Updated the Gemma3 model variant from `gemma3:4b` to `gemma3n:e4b` in
    the Ollama service configuration

--- nixos/llm.nix
+++ nixos/llm.nix
@@ -4,7 +4,7 @@
 
   services.ollama = {
     enable = true;
-    loadModels = [ "llama3.2:3b" "phi4-reasoning:14b" "dolphin3:8b" "smallthinker:3b" "gemma3:4b" "gemma3:12b" "gemma3:27b" "deepcoder:14b" "qwen3:14b" "nomic-embed-text" ];
+    loadModels = [ "llama3.2:3b" "phi4-reasoning:14b" "dolphin3:8b" "smallthinker:3b" "gemma3n:e4b" "gemma3:12b" "gemma3:27b" "deepcoder:14b" "qwen3:14b" "nomic-embed-text" ];
     acceleration = "cuda";
   };
 
