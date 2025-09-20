73d44cec110a43029bd3385d7d7fb55f29d213a0
xnm <artem.shv@proton.me>
2025-09-20 19:18:08 +0300

    fix(python): 🐛 Use `python313` instead of `python313Full`

--- nixos/programming-languages.nix
+++ nixos/programming-languages.nix
@@ -3,7 +3,7 @@
 {
   environment.systemPackages = with pkgs; [
     go
-    (python313Full.withPackages(ps: with ps; [ pygobject3 gobject-introspection pyqt6-sip]))
+    (python313.withPackages(ps: with ps; [ pygobject3 gobject-introspection pyqt6-sip]))
     uv
     nodePackages_latest.nodejs
     nodePackages_latest.pnpm
