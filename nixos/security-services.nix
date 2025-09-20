a64a0a69417f7ce06f7e4ab2fcc9f47e3d072e66
xnm <artem.shv@proton.me>
2025-09-20 19:17:36 +0300

    chore(security): 🔥 Remove `chkrootkit` package
    Removed chkrootkit from security tools as it's considered outdated and
    less effective compared to modern security scanning alternatives.

--- nixos/security-services.nix
+++ nixos/security-services.nix
@@ -100,7 +100,6 @@
   environment.systemPackages = with pkgs; [
     vulnix       #scan command: vulnix --system
     clamav       #scan command: sudo freshclam; clamscan [options] [file/directory/-]
-    chkrootkit   #scan command: sudo chkrootkit
 
     # passphrase2pgp
     pass-wayland
